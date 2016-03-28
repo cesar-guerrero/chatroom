2599e2b0fedbd3175f2624395ec10821ddc2ec99
Cesar Guerrero <cesarguerrero@Cesars-MacBook-Pro.local>
2016-03-28 19:50:21 -0400

    new mvn project

---
+++ src/main/java/com/shuffle/app/shuffle/App.java
@@ -0,0 +1,13 @@
+package com.shuffle.app.shuffle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
